97608593fcd3d3092b42963b5703dc8a68439061
rikves <skosalah1@gmail.com>
2025-03-22 02:03:27 +0000

    ldwsf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-  write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",9);
+  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
   return (1);
-}
+} 
