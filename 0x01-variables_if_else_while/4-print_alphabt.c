f80703cba05d464337e8d98118e62cf34039537e
rikves <skosalah1@gmail.com>
2025-03-22 03:14:33 +0000

    contnue

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 97;
+
+	while (c >= 97 && c <= 122)
+	{
+		if (c == 101 || c == 113)
+		§{
+			c++;
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
+
