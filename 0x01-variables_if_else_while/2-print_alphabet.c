4a42a9a29faa2647dcf0c62c9547a9eef0a1148f
rikves <skosalah1@gmail.com>
2025-03-22 02:24:41 +0000

    ldwsf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 97;
+
+	while (c >= 97 && c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(10);
+	return (0);
+}
+
