15cb41461f156031419e87bbf612ac49bf62de9a
rikves <skosalah1@gmail.com>
2025-04-13 02:23:55 +0100

    Done7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -7,4 +5,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+ 
