39b42fe635f641dcd5fb509c00e1019b3bd611a5
rikves <skosalah1@gmail.com>
2025-04-13 02:23:30 +0100

    Done6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints last digit
+ * @n: the number to be traited
+ * Return:(n)
+ */
+
+int print_last_digit(int n)
+{
+	int res;
+
+	if (n < 0)
+		n = -n;
+
+	res = n % 10;
+	_putchar(res + 48);
+
+	return (res);
+}
