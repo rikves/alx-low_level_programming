6e0afb693915105062b81a12f0410212833092bf
rikves <skosalah1@gmail.com>
2025-04-13 02:00:54 +0100

    Done6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "./main.h"
+
+/**
+ * _abs - the absolute value of n
+ * @n: int to be returned
+ * Return: (n)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);	
+}
