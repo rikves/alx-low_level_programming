7bede60747a76674f8e6a7e1b8867800a2e7f2bd
rikves <skosalah1@gmail.com>
2025-07-04 23:17:26 +0100

    10%

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "sevkir.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter and \
+ * the value it points to to 98.
+ *@n : int parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/sevkir.h
@@ -0,0 +1,9 @@
+#ifndef SEVKIR_H
+#define SEVKIR_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+void reset_to_98(int *n);
+
+#endif
