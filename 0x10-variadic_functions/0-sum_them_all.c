b680b8133835443b95ddb224eda914a7f7b593c2
salah-koulal <salah.rajawi289@gmail.com>
2025-11-16 12:05:14 +0100

    vaaa

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i;
-	int sum;
+	va_list ap;
+	unsigned int param, sum = 0;
 
-	sum = 0;
-	i = 0;
-	va_list args;
+	/* initialize the argument list from the start */
+	va_start(ap, n);
 
-	va_start(args, n);
-	while (i < n)
-	{
-		sum += va_arg(args, int);
-		i++;
-	}
-	va_end(args);
+	/* iterate through all parameter values*/
+	for (param = 0; param < n; param++)
+		/* get the next parameter value and add it to sum*/
+		sum += va_arg(ap, int);
+	/*Clean up*/
+	va_end(ap);
+
 	return (sum);
 }
